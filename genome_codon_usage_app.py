23e91c5c9742bd18ee0d351e9e8c73be28ecc33b
tobsecret <tobi.schraink@gmail.com>
2017-05-30 20:57:16 -0400

    + melt temperature ring plot; Efficiency fixes

--- genome_codon_usage_app.py
+++ genome_codon_usage_app.py
@@ -6,26 +6,36 @@ This is a simple in-browser app that:
 4. plots a scatter plot of the PCA data
 5. serves the codon distribution of a single selected data point in form of a bar diagram
 '''
-#Standard library
-import textwrap
+#Imports from standard library
+from math import pi
 
-#Other imports
+#Imports from other libraries
 import numpy as np
 import pandas as pd
 
 
-from Bio import SeqIO
+from Bio.SeqIO.FastaIO import SimpleFastaParser
+from Bio import SeqIO #this line should be removed once I have started using the SimpleFastaParser.
 from Bio.SeqUtils import CodonUsage, CodonUsageIndices
+from Bio.SeqUtils.MeltingTemp import Tm_GC
+
 
 from sklearn.decomposition import PCA
 from sklearn.preprocessing import StandardScaler
 
 from bokeh.layouts import row
-from bokeh.models import CategoricalColorMapper, HoverTool
+from bokeh.models import LinearColorMapper, CategoricalColorMapper, HoverTool
 from bokeh.models.sources import ColumnDataSource
 from bokeh.plotting import figure, curdoc
+from bokeh.palettes import Magma10
 
 
+def simple_string_wrap(string, width=50):
+    '''
+    Takes a single string and returns a generator of consecutive substrings
+    with a length of width.
+    '''
+    return (string[i:i+width] for i in range(0, len(string), width))
 
 #General settings:
 TOOLS="pan,tap,box_select,zoom_in,zoom_out,reset"
@@ -45,7 +55,7 @@ color_palette21 = ["#771155", "#114477", "#117777",
 
 #Importing and pre-processing the data
 smeg_cds = list(SeqIO.parse('Mycobacterium_smegmatis_coding_sequences_NC_008596.fasta.txt', 'fasta'))
-df = pd.DataFrame([pd.Series(textwrap.wrap(str(record.seq), width=3)).value_counts() 
+df = pd.DataFrame([pd.Series(simple_string_wrap(str(record.seq), width=3)).value_counts() 
                 for record in smeg_cds if (len(record.seq)%3==0)]
                 ).fillna(0) #Splitting all the records into their individual codons and counting how often each codon comes up in each record. NaNs are replaced with 0.
 df = df.loc[pd.Series([len(record.seq)%3 for record in smeg_cds])==0].astype(int) #dropping all coding sequences that have a number of bases that is not divisible by 3.
@@ -59,6 +69,8 @@ df_pca.columns = [str(col) for col in df_pca.columns.tolist()]
 joined = df_pca.join(df, how='outer')
 joined['gene'] = pd.Series([record.description.split('gene=')[1].split(']')[0] for record in smeg_cds])
 joined['gene_length'] = pd.Series([len(record.seq) for record in smeg_cds])
+
+smeg_genome = list(SeqIO.parse('Mycobacterium_smegmatis_whole_genome_NC_008596.fasta', 'fasta'))
 
 
 # Create the scatter plot
@@ -96,8 +108,51 @@ codon_dist.legend.location = (0,70)
 codon_dist.legend.border_line_color = None
 codon_dist.right.append(codon_dist.legend[0])
 
-layout = row(pca_scatter, codon_dist)
+#Create the ring plot
+seq_partition_length = 50000
+genome50mers = pd.Series(simple_string_wrap(str(smeg_genome[0].seq), width=seq_partition_length))
+tms_50mers =  genome50mers.apply(Tm_GC).reset_index().rename(columns={'index':'50mer_number',0:'melting temperature'})
 
+angle_increment = 2*pi/len(tms_50mers['50mer_number'])
+
+tms_50mers['end_angle'] = 0.5*pi-tms_50mers['50mer_number']*angle_increment
+tms_50mers['start_angle'] = tms_50mers['end_angle']-angle_increment
+tms_50mers['start_base'] = tms_50mers['50mer_number']*seq_partition_length+1
+tms_50mers['end_base'] = tms_50mers['start_base']+seq_partition_length-1
+tms_50mers.loc[tms_50mers.index[-1],'end_base'] = len(smeg_genome[0].seq) #The last end_base value should be identical to
+                                                                          #the length of the genome.
+
+tms_50mers_cd = ColumnDataSource(tms_50mers)
+tm_min = tms_50mers['melting temperature'].min()
+tm_max = tms_50mers['melting temperature'].max()
+
+melting_temp_color_mapper = LinearColorMapper(low=tm_min, high=tm_max,palette=Magma10)
+
+ring_plot = figure(
+                  tools='zoom_in,zoom_out,reset,pan', 
+                  plot_height=pca_scatter.plot_height, plot_width=400,
+                  title='Melting temperature of genome', 
+                  x_axis_location=None, y_axis_location=None
+                  )
+ring_plot.xgrid.grid_line_color = None
+ring_plot.ygrid.grid_line_color = None
+
+ring_plot.annular_wedge(x=1, y=1, inner_radius=0.25, outer_radius=0.5,
+                        start_angle='start_angle', 
+                        end_angle='end_angle', 
+                        direction='anticlock',
+                        color={'field':'melting temperature', 'transform':melting_temp_color_mapper}, 
+                        alpha=0.6, source=tms_50mers_cd)
+
+ring_hover = HoverTool(tooltips=[('start base','@start_base'), 
+                                 ('end base', '@end_base')
+                                ])
+ring_plot.add_tools(ring_hover)
+
+#Create the layout of the plots in the browser
+layout = row(ring_plot ,pca_scatter, codon_dist)
+
+#adding layout to the server environment
 curdoc().add_root(layout)
 curdoc().title = "Genome Explorer"
 
@@ -107,7 +162,7 @@ def update(attr, old, new):
     if len(inds) == 0:
         new_counts = np.zeros(64)
     else:
-        new_counts = joined.loc[:,codon_list].iloc[inds,:].sum().astype(int).reset_index().rename(columns={'index':'codon',0:'counts'})['counts'].tolist()
+        new_counts = joined.loc[:,codon_list].iloc[inds,:].sum().reset_index().rename(columns={'index':'codon',0:'counts'})['counts'].tolist()
 
     codon_hbar.data_source.data['counts'] = new_counts
     
